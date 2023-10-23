db96b99e4f316776906543a174aade1f0c32e693
Arber Qoku <arber.qoku@outlook.com>
2023-10-23 12:19:18 +0200

    Fix get_free_gpu test case

--- tests/test_get_free_gpu.py
+++ tests/test_get_free_gpu.py
@@ -1,12 +1,8 @@
-import pytest
 import torch
 from muvi import get_free_gpu_idx
 
 
-def test_early_stopping_callback():
+def test_get_free_gpu_idx():
     if torch.cuda.is_available():
         gpu_idx = get_free_gpu_idx()
         assert gpu_idx >= 0
-    else:
-        with pytest.raises(RuntimeError):
-            get_free_gpu_idx()
